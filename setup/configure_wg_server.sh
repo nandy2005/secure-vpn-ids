c554a53f570fc04877f80f6563c10163b324cef5
nandy2005 <asnandhana2005@gmail.com>
2025-10-18 10:05:44 +0530

    Changes in wireguard server configs

--- setup/configure_wg_server.sh
+++ setup/configure_wg_server.sh
@@ -11,7 +11,8 @@ SERVER_PRIV="$WG_DIR/server_private.key"
 SERVER_PUB="$WG_DIR/server_public.key"
 SERVER_PORT=51820
 VPN_SUBNET="10.8.0.0/24"
-NET_IFACE="enp0s3"
+NET_IFACE="enp0s8"
+CLIENT_PUBLIC_KEY=""
 
 echo "[+] Creating WireGuard config directory (if not exists)..."
 sudo mkdir -p "$WG_DIR"
@@ -34,6 +35,8 @@ PrivateKey = $(sudo cat $SERVER_PRIV)
 SaveConfig = true
 
 # (Clients will be added below as [Peer] sections)
+[Peer]
+PublicKey = 
 EOF
 
 echo "[+] Setting correct permissions..."
